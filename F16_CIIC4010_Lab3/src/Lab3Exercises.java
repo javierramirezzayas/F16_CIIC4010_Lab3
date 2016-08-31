746f70dbb1076f5acd1712e4ad2944e902e913a3
javierramirezzayas <javier.ramirez1@upr.edu>
2016-08-31 10:14:48 -0400

    Changed: -Line 8: Title name now is "Javier Ramirez Zayas". -Line 9: Removed comments. -Line 10: Position is different. -Line 11: Size is different.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	
 	
 	public static void main(String[] args) {
-
-		JFrame myFrame = new JFrame("Javier Ramirez Zayas"); //Changed from Hello,World! to Javier Ramirez Zayas.
+		
+		JFrame myFrame = new JFrame("Javier Ramirez Zayas"); //Changed from "Hello,World!" to Javier Ramirez Zayas.
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(600, 300); //Set Location to approximately center based on the computer screen.
 		myFrame.setSize(200, 200); //Set size to 200x200.
